f91e686886faea7f49acfb720832ad6dacedffdf
FlorOtero <otero.florencia96@gmail.com>
2016-11-24 14:05:28 +0000

    Conecting frames. New concert button go to New concert Frame, same with current concert.

--- src/concertbookingsystem/PrincipalFrame.java
+++ src/concertbookingsystem/PrincipalFrame.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package concertbookingsystem;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
 
 /**
  *
@@ -16,6 +20,24 @@ public class PrincipalFrame extends javax.swing.JFrame {
      */
     public PrincipalFrame() {
         initComponents();
+        jButton1.addActionListener
+                (new ActionListener()
+                {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        NewConcertFrame newConcert=new NewConcertFrame();
+                        newConcert.show();
+                    }
+                }) ;
+        jButton2.addActionListener
+                (new ActionListener()
+                {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        CurrentConcertFrame currentConcert=new CurrentConcertFrame();
+                        currentConcert.show();
+                    }
+                }) ;
     }
 
     /**
@@ -72,40 +94,6 @@ public class PrincipalFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PrincipalFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PrincipalFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PrincipalFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PrincipalFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new PrincipalFrame().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
